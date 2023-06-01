b57652f8292d32b0313084cd8eef3547b728d5e4
Majid-Al <mi.aliyan@gmail.com>
2023-06-01 14:51:40 +0330

    justURL

--- wp-config.php
+++ wp-config.php
@@ -83,7 +83,8 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('WP_HOME','http://localhost/Karamuzi/chakavak/');
+define('WP_SITEURL','http://localhost/Karamuzi/chakavak/');
 
 /* That's all, stop editing! Happy publishing. */
 
